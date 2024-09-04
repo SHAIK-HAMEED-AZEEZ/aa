400a8c15117e05c19000b2da2539678e304c4111
SHAIK-HAMEED-AZEEZ <shaikhameedazeez@gmail.com>
2024-09-04 22:29:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-// const hbs = require("hbs");
 const LogInCollection = require("./mongo");
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const tempelatePath = path.join(__dirname, "../tempelates");
+const templatePath = path.join(__dirname, "../templates");
 const publicPath = path.join(__dirname, "../public");
 console.log(publicPath);
 
 app.set("view engine", "hbs");
-app.set("views", tempelatePath);
+app.set("views", templatePath);
 app.use(express.static(publicPath));
 
-// hbs.registerPartials(partialPath);
-
 app.get("/signup", (req, res) => {
-  res.render("signup");
+  res.render("signup", { message: "" });
 });
+
 app.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", { message: "" });
 });
 
 app.get("/home", (req, res) => {
@@ -34,7 +32,7 @@ app.post("/signup", async (req, res) => {
     const existingUser = await LogInCollection.findOne({ name: req.body.name });
 
     if (existingUser) {
-      res.send("User details already exist");
+      res.render("signup", { message: "User details already exist" });
     } else {
       const data = new LogInCollection({
         name: req.body.name,
@@ -43,12 +41,10 @@ app.post("/signup", async (req, res) => {
 
       await data.save();
 
-      res.status(201).render("home", {
-        naming: req.body.name,
-      });
+      res.render("login", { message: "Sign-up successful, please log in." });
     }
   } catch (error) {
-    res.send("Wrong inputs");
+    res.render("signup", { message: "An error occurred. Please try again." });
   }
 });
 
@@ -59,10 +55,10 @@ app.post("/login", async (req, res) => {
     if (check && check.password === req.body.password) {
       res.status(201).render("home", { naming: req.body.name });
     } else {
-      res.send("Incorrect password");
+      res.render("login", { message: "Wrong password. Please try again." });
     }
   } catch (e) {
-    res.send("Wrong details");
+    res.render("login", { message: "User not found. Please sign up." });
   }
 });
 
